36a5fae033f404f8a7c91cfa3dc85d7f9c4e7ee3
Lisa Wagner <wagnerl@mskcc.org>
2020-08-14 15:56:42 -0400

    Update Template
    Frontend changes:
    - materialUI
    - CSS in JS(X)
    - generic marketplace layout, header
    - sorted imports (builtin, external. internal)
    - IGO favicon
    
    Backend changes:
    - babel-node, reload on file change
    
    Both:
    - .gitignore
    - updated dependencies
    - updated prettier rules

--- template-frontend/src/App.js
+++ template-frontend/src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import './App.css';
-import HomePage from './home-page';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './assets/igo-icon.png';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import HomePage from './home-page';
+import Header from './components/header';
+
+import './App.css';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: {
+      logo: '#319ae8',
+      light: '#8FC7E8',
+      main: '#007CBA',
+      dark: '#006098',
+    },
+    secondary: {
+      light: '#F6C65B',
+      main: '#DF4602',
+      dark: '#C24D00',
+    },
+
+    textSecondary: '#e0e0e0',
+  },
+});
 
 function App() {
-    return (<Router>
-        <header className='app-header background-mskcc-dark-blue'>
-            <p className={'mskcc-white em2'}>Something great coming...</p>
-        </header>
-        <div className={'background-mskcc-light-blue app-body'}>
-            <div className={'app-container'}>
-                <Switch>
-                    <Route path='/' component={HomePage}/>
-                </Switch>
-            </div>
-        </div>
-        <footer className='background-mskcc-dark-blue'>
-            <img alt='igo-logo' className='logo-footer' src={logo}></img>
-        </footer>
-    </Router>);
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/' component={HomePage} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
--- template-frontend/src/home-page.js
+++ template-frontend/src/home-page.js
@@ -1,53 +1,87 @@
-import React, {useState, useEffect} from 'react';
-import {getQOD, getQuote} from './services/quote';
+import React, { useState, useEffect } from 'react';
+import { getQOD, getQuote } from './services/quote';
+import { makeStyles, Button, Paper, Typography } from '@material-ui/core';
+
 import dna from './assets/dna.png';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: 'min-content',
+    padding: theme.spacing(4),
+    width: '70vw',
+    margin: '10em auto',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '3em',
+  },
+
+  quote: { marginBottom: theme.spacing(3) },
+
+  loadingIcon: {
+    height: '25px',
+    width: '25px',
+    animation: '$rotate 2s linear infinite',
+    transformOrigin: 'center center',
+    marginLeft: '20px',
+  },
+  
+  '@keyframes rotate': {
+    '100%': {
+      transform: 'rotate(360deg)',
+    },
+  },
+}));
+
 function HomePage() {
-    const [quoteData, setQuoteData] = useState({
-        quote: 'Science and everyday life cannot and should not be separated.',
-        author: 'Rosalind Franklin'
-    });
-    const [nextQuote, setNextQuote] = useState({});
+  const classes = useStyles();
 
-    const getNewQuote = (isRandom) => {
-        if(isRandom){
-            // Update w/ next quote, if data wasn't returned
-            setQuoteData(nextQuote || quoteData);
-            getNextQuote();
-        } else {
-            getQOD().then((newQuoteData) => {
-                // Add new quote, or use old quote, if data wasn't returned
-                setQuoteData(newQuoteData || quoteData)
-            });
-        }
-    };
+  const [quoteData, setQuoteData] = useState({
+    quote: 'Science and everyday life cannot and should not be separated.',
+    author: 'Rosalind Franklin',
+  });
+  const [nextQuote, setNextQuote] = useState({});
 
-    async function getNextQuote() {
-        getQuote().then(setNextQuote);
-    };
+  const getNewQuote = (isRandom) => {
+    if (isRandom) {
+      // Update w/ next quote, if data wasn't returned
+      setQuoteData(nextQuote || quoteData);
+      getNextQuote();
+    } else {
+      getQOD().then((newQuoteData) => {
+        // Add new quote, or use old quote, if data wasn't returned
+        setQuoteData(newQuoteData || quoteData);
+      });
+    }
+  };
 
-    useEffect(() => {
-        // Get Quote of Day first
-        getNextQuote(false);
-    }, []);
+  async function getNextQuote() {
+    getQuote().then(setNextQuote);
+  }
 
-    return (<div>
-        <div className={'quote-container'}>
-            <button type='button'
-                    onClick={() => getNewQuote(false)}>
-                <p>Quote of Day</p>
-            </button>
-            <button type='button'
-                    onClick={() => getNewQuote(true)}>
-                <p>Random</p>
-            </button>
-            <p className={'em4 italics mskcc-dark-blue'}>"{quoteData.quote || ''}"</p>
-            <div className={'width-350 float-right'}>
-                <p className={'em2 inline-text mskcc-dark-blue'}>- {quoteData['author'] || ''}</p>
-                <img alt='dna' className='loading-icon inline-text' src={dna}/>
-            </div>
-        </div>
-    </div>);
+  useEffect(() => {
+    // Get Quote of Day first
+    getNextQuote(false);
+  }, []);
+
+  return (
+    <Paper className={classes.container}>
+      <div className={classes.quote}>
+        <Typography variant='h2'>"{quoteData.quote || ''}"</Typography>
+        <Typography variant='h5' align='right'>
+          – {quoteData['author'] || ''}
+          <img alt='dna' className={classes.loadingIcon} src={dna} />
+        </Typography>
+      </div>
+      <div className={classes.buttons}>
+        <Button id='newQuote' onClick={() => getNewQuote(false)} color='primary' variant='contained' type='submit'>
+          Quote of Day
+        </Button>
+        <Button id='randomQuote' onClick={() => getNewQuote(true)} color='secondary' variant='contained' type='submit'>
+          Random Quote
+        </Button>
+      </div>
+    </Paper>
+  );
 }
 
 export default HomePage;
