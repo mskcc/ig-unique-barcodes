926dc6529889578655e967d44c2a03d1b1a35fcc
Lisa Wagner <wagnerl@mskcc.org>
2021-01-07 09:33:21 -0500

    Updates
    - updated package files
    - added more detailed winston config

--- template-backend/helpers/winston.js
+++ template-backend/helpers/winston.js
@@ -1,19 +1,42 @@
 const winston = require('winston');
-const {constants} = require('./constants');
 
-/**
- * Configures winston logger for application
- */
+const { format } = winston;
+const { constants } = require('./constants');
+
+const logLabel = constants.logger;
 const container = new winston.Container();
-const {format} = winston;
-const {combine, label, json} = format;
-container.add(constants.logger, {
-  format: combine(
-    label({label: 'App-Template'}),	// Added to log lines, e.g. { ..., "label":"App-Template"}
-    json()
-  ),
-  // "error" will always be logged to console
-  transports: [new winston.transports.Console({level: 'info'})]
+
+const fileConfig = format.combine(
+    format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.json()
+);
+
+const consoleConfig = format.combine(
+    format.colorize(),
+    format.align(),
+    format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+container.add(logLabel, {
+    format: fileConfig,
+    transports: [
+        new winston.transports.File({ filename: `${logLabel}_error.log`, level: 'error' }),
+        new winston.transports.File({ filename: `${logLabel}_combined.log` }),
+    ],
 });
 
-exports.logger = container.get(constants.logger);
+// nicely log to console for dev
+if (process.env.NODE_ENV === 'production') {
+    container.get(logLabel).add(
+        new winston.transports.Console({
+            format: consoleConfig,
+        })
+    );
+}
+
+exports.logger = container.get(logLabel);
