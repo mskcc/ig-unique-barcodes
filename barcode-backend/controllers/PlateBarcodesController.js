2350b89d2ef80a51a079d8aa292a912856ec0309
Stephanie DelBelso <sdelbelso@grubhub.com>
2022-11-16 15:13:28 -0500

    attempt to get route controller working

--- barcode-backend/controllers/PlateBarcodesController.js
+++ barcode-backend/controllers/PlateBarcodesController.js
@@ -1,4 +1,5 @@
 const apiResponse = require('../helpers/apiResponse');
+const barcodeModel = require('../models/BarcodeGenModel');
 const { generateUniqueBarcode, getCatFact } = require('../services/services');
 // const Cache = require('../helpers/cache');
 // const ttl = 60 * 60 * 1; // cache for 1 Hour
@@ -15,12 +16,48 @@ exports.generateUniqueBarcode = [
     console.log('info', 'Generating plate barcode');
     let plateType = req.query.plateType;
     let numberOfBarcodes = req.query.numOfBarcodes;
-    generateUniqueBarcode(plateType, numberOfBarcodes)
+
+    let listOfBarcodes = [];
+    console.log('plate type is: ' + plateType);
+    let year = new Date().getFullYear().toString().substring(2, 5);
+    console.log('Year is: ' + year);
+    let counter;
+    console.log('NumberOfBarcodes = ' + numberOfBarcodes);
+
+    let barcodeGeneratePromise = barcodeModel.findOne({plateType: plateType});
+    // generateUniqueBarcode(plateType, numberOfBarcodes)
+    Promise.all([barcodeGeneratePromise])
       .then((results) => {
-        if (!results) {
-          return apiResponse.errorResponse(res, `Could not generate barcodes.`);
+        const result = results[0];
+        console.log('After find.. Counter = ' + result.counter);
+        counter = result.counter;
+        let padded = false;
+        let prevCountOfTraillingZeros = 0;
+        for (let i = 0; i < numberOfBarcodes; i++) {
+          // MSK_DNA_2200001
+          console.log('in for loop counter = ' + counter);
+          newCounter = parseInt(counter) + 1;
+          counter = newCounter;
+          console.log('newCounter = ' + newCounter);
+          let countOfTraillingZeros = 5 - String(newCounter).length;
+          console.log('number or trailing zeros = ' + countOfTraillingZeros);
+          if (!padded && countOfTraillingZeros != prevCountOfTraillingZeros) {
+            year = year.padEnd(String(year).length + countOfTraillingZeros, '0');
+            prevCountOfTraillingZeros = countOfTraillingZeros;
+            padded = true;
+            console.log('year after padding to the end: ' + String(year))
+          }
+          let uniquePlateBarcode = String(plateType) + "_" + String(year) + newCounter;
+          listOfBarcodes.push(uniquePlateBarcode);
+          console.log('pushed ' + uniquePlateBarcode);
+          barcodeModel.findOneAndUpdate({plateType: plateType},{$set: { counter: newCounter }});
+          console.log('Updated the last index by one')
         }
-        return apiResponse.successResponseWithData(res, 'success', results);
+
+        // if (!results) {
+        //   return apiResponse.errorResponse(res, `Could not generate barcodes.`);
+        // }
+        return apiResponse.successResponseWithData(res, 'success', listOfBarcodes);
       })
       .catch((err) => {
         return apiResponse.errorResponse(res, err.message);
