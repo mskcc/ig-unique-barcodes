fc980d1ed98d21b23c188134718c90dae8d1d8bf
Mirhaj <f.mirhaj@gmail.com>
2022-12-14 14:02:06 -0500

    Removing unnecessary endpoint code

--- barcode-backend/controllers/PlateBarcodesController.js
+++ barcode-backend/controllers/PlateBarcodesController.js
@@ -30,23 +30,6 @@ exports.generateUniqueBarcode = [
   },
 ];
 
-exports.exportExcel = [
-  function (req, res) {
-    console.log('info', 'Exporting barcode into excel');
-    let barcodeList = req.query.barcodeList;
-    exportExcel(barcodeList)
-      .then((results) => {
-        if (!results) {
-          return apiResponse.errorResponse(res, 'Unble to export to excel.');
-        }
-        return apiResponse.successResponseWithData(res, 'success', results);
-      })
-      .catch((err) => {
-        return apiResponse.ErrorResponse(res, err.message);
-      });
-    },
-];
-
 exports.getNumOfBarcodes = [
   function (req, res) {
     if(typeof req.body.count === 'undefined'){
--- barcode-backend/routes/barcode.js
+++ barcode-backend/routes/barcode.js
@@ -3,6 +3,5 @@ const PlateBarcodesController = require('../controllers/PlateBarcodesController'
 var router = express.Router();
 
 router.get('/plateBarcode', PlateBarcodesController.generateUniqueBarcode);
-router.get('/exportExcel', PlateBarcodesController.exportExcel);
 
 module.exports = router;
